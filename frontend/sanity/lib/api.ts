1b4967d11cfaa4bdd6566cef645b9e7b18912d8b
Kevin <kevin.vo@enginedigital.com>
2025-10-02 13:46:21 -0400

    chore: biome lint

--- frontend/sanity/lib/api.ts
+++ frontend/sanity/lib/api.ts
@@ -1,7 +1,7 @@
 /*
-  * As this file is reused in several other files, try to keep it lean and small.
-  * Importing other npm packages here could lead to needlessly increasing the client bundle size, or end up in a server-only function that don't need it.
-*/
+ * As this file is reused in several other files, try to keep it lean and small.
+ * Importing other npm packages here could lead to needlessly increasing the client bundle size, or end up in a server-only function that don't need it.
+ */
 
 function assertValue<T>(v: T | undefined, errorMessage: string): T {
   if (v === undefined) {
---
+++ studio/src/schemas/objects/callToAction.ts
@@ -0,0 +1,56 @@
+import { BulbOutlineIcon } from '@sanity/icons';
+
+import { portableTextPlain } from '@/schemas/sharedFields';
+
+import { defineField, defineType } from 'sanity';
+
+/**
+ * Call to action schema object.  Objects are reusable schema structures document.
+ * Learn more: https://www.sanity.io/docs/object-type
+ */
+
+export const callToAction = defineType({
+  name: 'callToAction',
+  title: 'Call to Action',
+  type: 'object',
+  icon: BulbOutlineIcon,
+  fields: [
+    defineField({
+      name: 'heading',
+      title: 'Heading',
+      type: 'string',
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: 'text',
+      title: 'Text',
+      type: 'text',
+    }),
+    defineField({
+      name: 'buttonText',
+      title: 'Button text',
+      type: 'string',
+    }),
+    defineField({
+      name: 'cta',
+      title: 'Button Link',
+      type: 'cta',
+    }),
+    defineField({
+      ...portableTextPlain,
+    }),
+  ],
+  preview: {
+    select: {
+      title: 'heading',
+    },
+    prepare(selection) {
+      const { title } = selection;
+
+      return {
+        title: title,
+        subtitle: 'Call to Action',
+      };
+    },
+  },
+});
