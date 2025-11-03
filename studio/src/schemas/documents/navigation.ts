bfbdcf0ca34d7a68adbb2278478761e7f70efcb8
Kevin <kevin.vo@enginedigital.com>
2025-11-03 11:38:04 -0500

    feat: update navigation schema

--- studio/src/schemas/documents/navigation.ts
+++ studio/src/schemas/documents/navigation.ts
@@ -1,26 +1,117 @@
-import { ComponentIcon } from "@sanity/icons";
-import { cta, description, image, title } from "@/schemas/sharedFields";
+import { LinkIcon } from "@sanity/icons";
+import { cta, image, logo, title } from "@/schemas/sharedFields";
 import { defineField, defineType } from "sanity";
-
-// Remember to change the icon to suit your new schema
 
 export const navigation = defineType({
 	name: "navigation",
 	title: "Navigation",
 	type: "document",
-	icon: ComponentIcon,
+	icon: LinkIcon,
 	fields: [
 		defineField({
-			...title,
+			name: "mainLinks",
+			title: "Main Links",
+			type: "array",
+			of: [
+				{
+					name: "mainLinkGroup",
+					title: "Main Link Group",
+					type: "object",
+					icon: LinkIcon,
+					fields: [
+						defineField({
+							...title,
+							title: "Main Link Title",
+						}),
+						defineField({
+							...image,
+							options: { collapsible: true, collapsed: true },
+							validation: (Rule) => Rule,
+						}),
+						defineField({
+							name: "subLinks",
+							title: "Sub Links",
+							type: "array",
+							of: [
+								{
+									name: "subLink",
+									title: "Sub Link",
+									type: "object",
+									icon: LinkIcon,
+									fields: [
+										defineField({
+											...logo,
+										}),
+										defineField({
+											...cta,
+										}),
+									],
+									preview: {
+										select: {
+											title: "cta.label",
+											logo: "logo",
+										},
+										prepare({ title, logo }) {
+											return {
+												title: title,
+												media: logo,
+											};
+										},
+									},
+								},
+							],
+						}),
+					],
+					preview: {
+						select: {
+							title: "title",
+							image: "image",
+							subLinks: "subLinks",
+						},
+						prepare({ title, subLinks, image }) {
+							const subLinkTitles = subLinks
+								?.map(
+									(subLink: { cta: { label: string } }) => subLink?.cta?.label,
+								)
+								.filter(Boolean);
+							return {
+								title: title,
+								media: image,
+								subtitle: subLinkTitles?.join(", ") || "",
+							};
+						},
+					},
+				},
+			],
 		}),
 		defineField({
-			...description,
-		}),
-		defineField({
-			...image,
-		}),
-		defineField({
-			...cta,
+			name: "extraLinks",
+			title: "Extra Links",
+			type: "array",
+			of: [
+				{
+					name: "mainLinkGroup",
+					title: "Main Link Group",
+					type: "object",
+					icon: LinkIcon,
+					fields: [
+						defineField({
+							...cta,
+							title: "Main Link Title",
+						}),
+					],
+					preview: {
+						select: {
+							title: "cta.label",
+						},
+						prepare({ title }) {
+							return {
+								title: title,
+							};
+						},
+					},
+				},
+			],
 		}),
 	],
 	preview: {
