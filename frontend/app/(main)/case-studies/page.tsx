3ff244fe11aa309b924101f5eaa82b85e7c069e5
Kevin <kevin.vo@enginedigital.com>
2026-02-01 00:04:54 -0500

    feat: add post data

--- frontend/app/(main)/case-studies/page.tsx
+++ frontend/app/(main)/case-studies/page.tsx
@@ -1,7 +1,10 @@
 import { notFound } from "next/navigation";
 import { FETCH_CONTENT_TYPE_INDEX_PAGE_DATA } from "@/sanity/lib/fetch";
+import { Transition } from "@/components/animations/Transition";
 import JSONLDScript from "@/components/layout/JsonLD/Jsonld";
 import { ModuleBuilder } from "@/components/modules/ModuleBuilder";
+import { Button } from "@/components/primitives/Button/Button";
+import { PortableTextFragment } from "@/components/primitives/PortableText/PortableText";
 import { generatePageMetadata } from "@/lib/site/metadata";
 
 const CONTENT_TYPE = "case-studies-index";
@@ -41,8 +44,46 @@ export default async function BlogIndexPage(props: {
 
   return (
     <>
-      CASE STUDIESPAGE
       <JSONLDScript document={data} />
+
+      <div className="flex flex-col gap-y-10 p-custom py-20">
+        <header className="">
+          {data.title && (
+            <h1>
+              <PortableTextFragment value={data.title} />
+            </h1>
+          )}
+          {data.description && (
+            <p>
+              <PortableTextFragment value={data.description} />
+            </p>
+          )}
+        </header>
+        <ul className="grid-custom">
+          {data?.posts?.map((post) => (
+            <li key={post._id} className="col-span-3">
+              <Transition className="flex flex-col gap-y-20 rounded bg-white p-4 text-black">
+                {post._createdAt && (
+                  <p className="">
+                    {new Date(post._createdAt).toLocaleDateString()}
+                  </p>
+                )}
+                <Button
+                  href={`/blog/${post.slug.current}`}
+                  variant="ghost"
+                  card
+                  hasArrow={false}
+                  width="fit"
+                >
+                  <PortableTextFragment value={post.title} />
+                </Button>
+              </Transition>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      {/* Remove the ModuleBuilder if you do not need to render specific modules here like Driver modules */}
       <ModuleBuilder modules={data?.modules ?? []} />
     </>
   );
