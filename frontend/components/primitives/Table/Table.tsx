a5cb5f5f74e21cbaa7772b3510fb8b8d49394c16
Kevin <kevin.vo@enginedigital.com>
2026-02-24 13:43:24 -0500

    feat: add Table primitive

---
+++ frontend/components/primitives/Table/Table.tsx
@@ -0,0 +1,170 @@
+"use client";
+
+import type * as React from "react";
+import { cn } from "@/lib/utils/cn";
+
+/*
+ * Table component for displaying tabular data with header, body, and footer
+ *
+ * Configure default styling here.
+ * Remember that styles defined here must be generic as they will be applied to all Table components.
+ * If your style is unique, you can override the styles by passing into `className` prop.
+ * Otherwise, you can conditionally render or apply styles in here by extending props.
+ *
+ * ---------------------
+ * Usage Example: Basic
+ * ---------------------
+ * <TableRoot>
+ *   <TableHeader>
+ *     <TableRow>
+ *       <TableHead>Header 1</TableHead>
+ *       <TableHead>Header 2</TableHead>
+ *     </TableRow>
+ *   </TableHeader>
+ *   <TableBody>
+ *     <TableRow>
+ *       <TableCell>Data 1</TableCell>
+ *       <TableCell>Data 2</TableCell>
+ *     </TableRow>
+ *   </TableBody>
+ * </TableRoot>
+ *
+ * ---------------------
+ * Usage Example: With Caption and Footer
+ * ---------------------
+ * <TableRoot>
+ *   <TableCaption>A list of your recent invoices.</TableCaption>
+ *   <TableHeader>
+ *     <TableRow>
+ *       <TableHead>Invoice</TableHead>
+ *       <TableHead>Status</TableHead>
+ *       <TableHead>Method</TableHead>
+ *       <TableHead className="text-right">Amount</TableHead>
+ *     </TableRow>
+ *   </TableHeader>
+ *   <TableBody>
+ *     <TableRow>
+ *       <TableCell className="font-medium">INV001</TableCell>
+ *       <TableCell>Paid</TableCell>
+ *       <TableCell>Credit Card</TableCell>
+ *       <TableCell className="text-right">$250.00</TableCell>
+ *     </TableRow>
+ *   </TableBody>
+ *   <TableFooter>
+ *     <TableRow>
+ *       <TableCell colSpan={3}>Total</TableCell>
+ *       <TableCell className="text-right">$2,500.00</TableCell>
+ *     </TableRow>
+ *   </TableFooter>
+ * </TableRoot>
+ */
+
+function TableRoot({ className, ...props }: React.ComponentProps<"table">) {
+  return (
+    <div
+      data-slot="table-container"
+      className="relative w-full overflow-x-auto"
+    >
+      <table
+        data-slot="table"
+        className={cn("w-full caption-bottom", className)}
+        {...props}
+      />
+    </div>
+  );
+}
+
+function TableHeader({ className, ...props }: React.ComponentProps<"thead">) {
+  return (
+    <thead
+      data-slot="table-header"
+      className={cn("[&_tr]:border-b", className)}
+      {...props}
+    />
+  );
+}
+
+function TableBody({ className, ...props }: React.ComponentProps<"tbody">) {
+  return (
+    <tbody
+      data-slot="table-body"
+      className={cn("[&_tr:last-child]:border-0", className)}
+      {...props}
+    />
+  );
+}
+
+function TableFooter({ className, ...props }: React.ComponentProps<"tfoot">) {
+  return (
+    <tfoot
+      data-slot="table-footer"
+      className={cn("border-t [&>tr]:last:border-b-0", className)}
+      {...props}
+    />
+  );
+}
+
+function TableRow({ className, ...props }: React.ComponentProps<"tr">) {
+  return (
+    <tr
+      data-slot="table-row"
+      className={cn(
+        "border-b transition-colors hover:bg-white/10 data-[state=selected]:bg-white/10",
+        className,
+      )}
+      {...props}
+    />
+  );
+}
+
+function TableHead({ className, ...props }: React.ComponentProps<"th">) {
+  return (
+    <th
+      data-slot="table-head"
+      className={cn(
+        "whitespace-nowrap text-left align-middle [&:has([role=checkbox])]:pr-0",
+        "p-2",
+        className,
+      )}
+      {...props}
+    />
+  );
+}
+
+function TableCell({ className, ...props }: React.ComponentProps<"td">) {
+  return (
+    <td
+      data-slot="table-cell"
+      className={cn(
+        "whitespace-nowrap align-middle [&:has([role=checkbox])]:pr-0",
+        "p-2",
+        className,
+      )}
+      {...props}
+    />
+  );
+}
+
+function TableCaption({
+  className,
+  ...props
+}: React.ComponentProps<"caption">) {
+  return (
+    <caption
+      data-slot="table-caption"
+      className={cn("mt-4", className)}
+      {...props}
+    />
+  );
+}
+
+export {
+  TableRoot,
+  TableHeader,
+  TableBody,
+  TableFooter,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableCaption,
+};
