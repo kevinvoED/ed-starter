9b85f2ff044ac88a7b5c698e69e2e2c1d647c445
Kevin <kevin.vo@enginedigital.com>
2026-02-25 14:21:39 -0500

    feat: add skip to main content + Banner

--- frontend/app/(main)/layout.tsx
+++ frontend/app/(main)/layout.tsx
@@ -4,6 +4,7 @@ import { SanityLive } from "@/sanity/lib/live";
 import { DisableDraftMode } from "@/components/layout/DraftMode/DisableDraftMode";
 import { Footer } from "@/components/layout/Footer/Footer";
 import { Header } from "@/components/layout/Header/Header";
+import { Banner } from "@/components/primitives/Banner/Banner";
 
 export default async function MainLayout({
   children,
@@ -12,6 +13,14 @@ export default async function MainLayout({
 }) {
   return (
     <>
+      <a
+        href="#main"
+        id="skipToMain"
+        className="absolute top-4 left-4 z-9999 -translate-y-16 bg-white px-3 py-1.5 text-black transition focus:translate-y-0"
+      >
+        Skip to main content
+      </a>
+      <Banner />
       <Header />
       <main>{children}</main>
       <SanityLive />
