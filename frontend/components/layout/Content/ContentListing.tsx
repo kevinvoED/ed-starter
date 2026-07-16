c468dd913bf75008e8843e56ed820ba9a48669e5
Kevin <kevin.vo@enginedigital.com>
2026-07-16 15:42:46 -0400

    refactor: create content post file

--- frontend/components/layout/Content/ContentListing.tsx
+++ frontend/components/layout/Content/ContentListing.tsx
@@ -1,10 +1,5 @@
 import type { ContentIndexVariant } from "@/lib/utils/types";
-import { toPlainText } from "next-sanity";
-import { Transition } from "@/components/animations/Transition";
-import { Eyebrow } from "@/components/primitives/Eyebrow/Eyebrow";
-import { SanityImage } from "@/components/primitives/Image/SanityImage";
-import { SanityLink } from "@/components/primitives/Link/SanityLink";
-import { PortableText } from "@/components/primitives/PortableText/PortableText";
+import { ContentPost } from "./ContentPost";
 import { cn } from "cnfast";
 
 type ContentListingProps = {
@@ -13,12 +8,11 @@ type ContentListingProps = {
   className?: string;
 };
 
-export const ContentListing = ({
+export const ContentListing = async ({
   scrollTargetId,
   posts,
   className,
 }: ContentListingProps) => {
-  // If no posts, return a properly designed EmptyState here instead of null
   if (!posts || posts.length === 0) return null;
 
   return (
@@ -26,63 +20,8 @@ export const ContentListing = ({
       id={scrollTargetId}
       className={cn("grid-custom col-span-full xl:col-span-10", className)}
     >
-      {posts?.map((post, index) => (
-        <li
-          key={post._id}
-          className="col-span-full rounded-lg md:col-span-2 lg:col-span-6 xl:col-span-4"
-        >
-          <Transition
-            delay={0.15 * index}
-            className="h-full bg-white ring ring-black/5"
-          >
-            <SanityLink
-              id="cta"
-              href={post.href}
-              variant="ghost"
-              card={true}
-              width="fit"
-              className="h-full"
-            >
-              <div className="flex h-full flex-col justify-between">
-                {post.image && (
-                  <SanityImage
-                    image={post.image}
-                    sizes="(max-width: 768px) 100vw, 33vw"
-                    className="h-60 overflow-hidden rounded-tl-lg rounded-tr-lg border-black/10 border-b"
-                  />
-                )}
-
-                <div className="flex flex-col gap-4 px-2.5 py-6">
-                  <div className="flex flex-wrap gap-1">
-                    {post._createdAt && (
-                      <Eyebrow variant="filter">
-                        {new Date(post._createdAt).toLocaleDateString()}
-                      </Eyebrow>
-                    )}
-
-                    {post.category?.map(({ _id, title }) => (
-                      <Eyebrow key={_id} variant="filter">
-                        {toPlainText(title)}
-                      </Eyebrow>
-                    ))}
-
-                    {post.contentTopic?.map(({ _id, title }) => (
-                      <Eyebrow key={_id} variant="filter">
-                        {toPlainText(title)}
-                      </Eyebrow>
-                    ))}
-                  </div>
-
-                  <PortableText
-                    as="h3"
-                    value={post.title}
-                    className="type-body-1650"
-                  />
-                </div>
-              </div>
-            </SanityLink>
-          </Transition>
-        </li>
+      {posts.map((post, index) => (
+        <ContentPost key={post._id} post={post} index={index} />
       ))}
     </ul>
   );
---
+++ frontend/components/layout/Content/ContentPost.tsx
@@ -0,0 +1,70 @@
+import type { ContentIndexVariant } from "@/lib/utils/types";
+import { toPlainText } from "next-sanity";
+import { Transition } from "@/components/animations/Transition";
+import { Eyebrow } from "@/components/primitives/Eyebrow/Eyebrow";
+import { SanityImage } from "@/components/primitives/Image/SanityImage";
+import { SanityLink } from "@/components/primitives/Link/SanityLink";
+import { PortableText } from "@/components/primitives/PortableText/PortableText";
+
+type ContentPostProps = {
+  post: ContentIndexVariant["posts"][number];
+  index: number;
+};
+
+export const ContentPost = ({ post, index }: ContentPostProps) => {
+  return (
+    <li className="col-span-full rounded-lg md:col-span-2 lg:col-span-6 xl:col-span-4">
+      <Transition
+        delay={0.15 * index}
+        className="h-full bg-white ring ring-black/5"
+      >
+        <SanityLink
+          id="cta"
+          href={post.href}
+          variant="ghost"
+          card={true}
+          width="fit"
+          className="h-full"
+        >
+          <div className="flex h-full flex-col justify-between">
+            {post.image && (
+              <SanityImage
+                image={post.image}
+                sizes="(max-width: 768px) 100vw, 33vw"
+                className="h-60 overflow-hidden rounded-tl-lg rounded-tr-lg border-black/10 border-b"
+              />
+            )}
+
+            <div className="flex flex-col gap-4 px-2.5 py-6">
+              <div className="flex flex-wrap gap-1">
+                {post._createdAt && (
+                  <Eyebrow variant="filter">
+                    {new Date(post._createdAt).toLocaleDateString()}
+                  </Eyebrow>
+                )}
+
+                {post.category?.map(({ _id, title }) => (
+                  <Eyebrow key={_id} variant="filter">
+                    {toPlainText(title)}
+                  </Eyebrow>
+                ))}
+
+                {post.contentTopic?.map(({ _id, title }) => (
+                  <Eyebrow key={_id} variant="filter">
+                    {toPlainText(title)}
+                  </Eyebrow>
+                ))}
+              </div>
+
+              <PortableText
+                as="h3"
+                value={post.title}
+                className="type-body-1650"
+              />
+            </div>
+          </div>
+        </SanityLink>
+      </Transition>
+    </li>
+  );
+};
